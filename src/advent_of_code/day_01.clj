f734ce248ebb2723d49f57234a9f0d99d234edc8
Alexander Hoffert <alexander.hoffert@sap.com>
2023-12-04 09:41:53 +0100

    day 1 and 2

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,78 @@
-(ns advent-of-code.day-01)
+(ns advent-of-code.day-01
+  (:require [clojure.string :as string]))
+
+(def min-value (int \0))
+(def max-value (int \9))
+
+(defn number-or-nil [char]
+  (let [code (int char)]
+    (when (and
+           (>= code min-value)
+           (<= code max-value))
+      char)))
+
+(defn first-number [line]
+  (some number-or-nil line))
+
+(defn last-number [line]
+  (some number-or-nil (reverse line)))
+
+(defn line-value [line]
+  (->> line
+       (#(str (first-number %) (last-number %)))
+       Integer/parseInt))
+
+(defn values [lines]
+  (map line-value lines))
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (->> input
+       string/split-lines
+       values
+       (reduce +)))
+
+(def digits {"one" 1
+             "two" 2
+             "three" 3
+             "four" 4
+             "five" 5
+             "six" 6
+             "seven" 7
+             "eight" 8
+             "nine" 9})
+
+
+(def digit-regex
+  (as-> digits _
+    (keys _)
+    (string/join "|" _)
+    (str "(?=(\\d|" _ "))")
+    (re-pattern _)))
+
+(defn to-digit [value]
+  (if (contains? digits value)
+    (get digits value)
+    value))
+
+(defn get-result-value [values]
+  (Integer/parseInt (str (first values) (last values))))
+
+(defn real-line-value [line]
+  (->> line
+       (re-seq digit-regex)
+       (map last)
+       (map to-digit)
+       get-result-value))
+
+(defn real-values [lines]
+  (map real-line-value lines))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (->> input
+       string/split-lines
+       real-values
+       (reduce +)))
--- src/advent_of_code/day_02.clj
+++ src/advent_of_code/day_02.clj
@@ -1,11 +1,69 @@
-(ns advent-of-code.day-02)
+(ns advent-of-code.day-02
+  (:require [clojure.string :as string]))
+
+(def game-regex #"Game (\d+):(.+)")
+(def pick-regex #"(\d+) (\w+)")
+(def limits {"red" 12
+             "green" 13
+             "blue" 14})
+
+(defn parse-pick [text]
+  (->> text
+       (re-seq pick-regex)
+       (mapcat #(list (get % 2) (Integer/parseInt (get % 1))))
+       (apply hash-map)))
+
+(defn parse-picks [text]
+  (as-> text _
+    (string/split _ #";")
+    (map parse-pick _)))
+
+(defn parse-line [line]
+  (as-> line _
+    (re-find game-regex _)
+    (rest _)
+    {:game (Integer/parseInt (first _)) :picks (parse-picks (second _))}))
+
+(defn is-valid [pick]
+  (every?
+   #(or (not (contains? pick %))
+        (<= (get pick %) (get limits %)))
+   (keys pick)))
+
+(defn check-picks [picks]
+  (every? #(is-valid %) picks))
+
+(defn valid-picks [game]
+  (-> game
+      (get :picks)
+      check-picks))
+
+(defn highest-amount [picks key]
+  (->> picks
+       (filter #(contains? % key))
+       (map #(get % key))
+       (reduce max 1)))
+
+(defn power [game]
+  (as-> (get game :picks) _
+    (map #(highest-amount _ %) (keys limits))
+    (reduce * _)))
 
 (defn part-1
   "Day 02 Part 1"
   [input]
-  input)
+  (->> input
+       string/split-lines
+       (map parse-line)
+       (filter valid-picks)
+       (map #(get % :game))
+       (reduce +)))
 
 (defn part-2
   "Day 02 Part 2"
   [input]
-  input)
+  (->> input
+       string/split-lines
+       (map parse-line)
+       (map power)
+       (reduce +)))
